cbf8915f896c31b74003986d59d1b42c362826fb
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-24 20:13:32 +0600

    basic setup added

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+ 
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+    
+    </div>
+  );
+};
+
+export default Home;
+
