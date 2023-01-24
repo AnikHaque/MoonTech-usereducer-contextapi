c0b9b042b7254bfd03b7429719a1400ea6c01202
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-24 20:24:05 +0600

    data fetching using context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 
 const Home = () => {
- 
+ const {data} = useProducts();
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-    
+    {
+      data.map(product => <ProductCard product={product} />)
+    }
     </div>
   );
 };
